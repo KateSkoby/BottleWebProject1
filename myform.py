b6f6cb2355874718eb3c32e05edf9c9d2e7744a2
KateSkoby <katesko@yandex.ru>
2023-04-26 11:24:18 +0300

    dictionary [username, question]

--- myform.py
+++ myform.py
@@ -6,13 +6,16 @@ import pdb
 
 @post('/home', method='post')
 def my_form():
+    questions = {}
     question = request.forms.get('QUEST')
     name = request.forms.get('USERNAME')
     mail = request.forms.get('ADRESS')
     nameStr = "%s" % name
     mailStr = "%s" % mail
+    questionStr = "%s" % question
+    questions["%s" % mail] = "%s" % question
 
-    if len("%s" % question) < 1:
+    if len(questionStr) < 1:
         return '''
                 <h3> Ask a Question </h3>
                 <form action="/home" method="post">
@@ -23,7 +26,7 @@ def my_form():
                     <p><input type="submit" value="Send" class="btn btn-default"></p>
                 </form>
                '''
-    if "%s" % name == None:
+    if len(nameStr) < 1:
         return '''
                 <dialog id="no_message" open>    
                     Enter your name
@@ -36,7 +39,7 @@ def my_form():
                     <p><input type="submit" value="Send" class="btn btn-default"></p>
                 </form>
                '''
-    if len("%s" % mail) < 1:
+    if len(mailStr) < 1:
         return '''
                 <h3> Ask a Question </h3>
                 <form action="/home" method="post">
@@ -47,10 +50,11 @@ def my_form():
                     <p><input type="submit" value="Send" class="btn btn-default"></p>
                 </form>
                '''
-    regex = re.fullmatch(r'[a-z0-9]{2,25}@[a-z]{2,9}\.(com|ru)',"%s" % mail)
-    if len("%s" % name) > 2:
+    regex = re.fullmatch(r'[a-z0-9]{2,25}@[a-z]{2,9}\.(com|ru)', mailStr)
+    if len(nameStr) > 2:
         if regex:
-            questions = {mail: question}
+            questions = {nameStr: questionStr}
+            print(questions)
             pdb.set_trace()
             return "Thanks, " + nameStr + "! The answer will be sent to the mail " + mailStr + " (date: " + str(datetime.date.today()) + ")"
         return '''
